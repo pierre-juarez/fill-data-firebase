457da743f1ed2ae5416edfe8ee99c5a5a7592605
Pierre Juarez <76144339+pierre-juarez@users.noreply.github.com>
2023-01-15 03:08:18 -0500

    [add] - Filling of data in Firebase 🥳

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ function getItemsByCollection(collectionName){
   collectionRef.get()
     .then(snapshot => {
       snapshot.forEach(doc => {
-        console.log(doc.id, '=>', doc.data());
+        console.log(doc.id, '=>', doc.data().arrayDates);
       });
     })
     .catch(error => {
@@ -32,4 +32,35 @@ function getItemsByCollection(collectionName){
     });
 }
 
+function insertData(collectionName, docId, data){
+  const collectionRef = db.collection(collectionName);
+
+  collectionRef.doc(docId).update({
+    arrayDates: admin.firestore.FieldValue.arrayUnion(data)
+  })
+  .then(() => {
+    console.log("El elemento ha sido agregado exitosamente a la propiedad arrayDates");
+    
+  })
+  .catch(error => {
+    console.log(`Error al agregar el elemento a la propiedad arrayDates: ${error}`);
+  });
+  
+}
+
 getAllCollections();
+
+const newElement = {
+  name: "Nueva actividad",
+  place: new admin.firestore.GeoPoint(37.78, -122.41),
+  realized: false,
+  suggestions: ["Opción 1", "Opción 2", "Opción 3"],
+  updated_by: "Pierre",
+  updated: admin.firestore.Timestamp.fromDate(new Date()),
+  created: admin.firestore.Timestamp.fromDate(new Date()),
+  possible_location: "Parque de atracciones",
+  photo_url: "https://example.com/foto.jpg",
+  number: 3
+};
+
+insertData('nameCollection','docName', newElement);
