116d75230fa7be0823003f1b005f2ee14c3c06b3
Pierre Juarez <76144339+pierre-juarez@users.noreply.github.com>
2023-01-15 20:01:47 -0500

    [add] - Filling of data multiple in Firebase 👀

--- index.js
+++ index.js
@@ -24,7 +24,8 @@ function getItemsByCollection(collectionName){
   collectionRef.get()
     .then(snapshot => {
       snapshot.forEach(doc => {
-        console.log(doc.id, '=>', doc.data().arrayDates);
+        const sortedArrayDates = doc.data().arrayDates.sort((a, b) => a.number - b.number);
+        console.log(sortedArrayDates);
       });
     })
     .catch(error => {
@@ -39,13 +40,48 @@ function insertData(collectionName, docId, data){
     arrayDates: admin.firestore.FieldValue.arrayUnion(data)
   })
   .then(() => {
-    console.log("El elemento ha sido agregado exitosamente a la propiedad arrayDates");
+    console.log(data.name,"El elemento ha sido agregado exitosamente a la propiedad arrayDates");
     
   })
   .catch(error => {
     console.log(`Error al agregar el elemento a la propiedad arrayDates: ${error}`);
   });
   
+}
+
+function insertMultipleData(collectionName, docName){
+  const activityNames = [
+    "Día de picnic",
+    "Cenar en un restaurante bonito",
+    "Viaje fuera de la ciudad",
+    "Ir a un Museo",
+    "Pedir cena a domicilio",
+    "Armar un rompecabezas",
+    "Preparar un postre",
+    "Pintar un cuadro",
+    "Ir a patinar",
+    "Hacer un pastel/galletas",
+    "Hacer un maratón de series o películas",
+    "Ir a la playa"
+  ];
+  let acum = 3;
+  activityNames.forEach((activity) => {
+    acum += 1;
+    const newElement = {
+      name: activity,
+      place: new admin.firestore.GeoPoint(37.78, -122.41),
+      realized: false,
+      suggestions: ["Opción 1", "Opción 2", "Opción 3"],
+      updated_by: "Pierre",
+      updated: admin.firestore.Timestamp.fromDate(new Date()),
+      created: admin.firestore.Timestamp.fromDate(new Date()),
+      possible_location: "",
+      photo_url: "https://example.com/foto.jpg",
+      number: acum
+    };
+    insertData(collectionName, docName, newElement);
+  });
+
 }
 
 getAllCollections();
@@ -63,4 +99,5 @@ const newElement = {
   number: 3
 };
 
-insertData('nameCollection','docName', newElement);
+//insertData('nameCollection','docName', newElement);
+insertMultipleData('nameCollection', 'docName');
